a6604cc41cc0575284a0123f64db0caca1ac60fc
Gabriela Duran <gabrieladuran013@gmail.com>
2024-08-24 22:59:17 -0300

    oneDay oneProject

--- 2-CalculadorEdad/index.js
+++ 2-CalculadorEdad/index.js
@@ -1,12 +1,25 @@
 
 
-let age = ''
-let dateToday = new Date
-console.log(dateToday)
+
+const date = new Date();
+const [month, day, year] = [
+  date.getMonth() + 1,
+  date.getDate(),
+  date.getFullYear(),
+];
+const dateToday = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
 
 
-function dateCalculation(){
-    dateBirthday = document.querySelector('#dateBirthday').value
-    
+
+function dateCalculation() {
+    const dateBirthday = document.querySelector('#dateBirthday').value;
+    const dateTodayObj = new Date(dateToday);
+    const dateBirthdayObj = new Date(dateBirthday);
+
+    const ageInMilliseconds = dateTodayObj - dateBirthdayObj;
+    const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25); 
+
+    const age = Math.floor(ageInYears);
+    document.getElementById('result').textContent = `${age}`;
 
 }
